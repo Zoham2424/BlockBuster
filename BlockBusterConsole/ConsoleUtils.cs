c1068f49213380840d5cf24eea4d4751ec181a76
Ian A <iaalvaradocubero@email.neit.edu>
2025-02-07 08:02:30 -0500

    Week 4

---
+++ BlockBusterConsole/ConsoleUtils.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BlockBuster.Models;
+using CsvHelper;
+
+namespace BlockBuster.ConsoleApp
+{
+    public class ConsoleUtils
+    {
+        public static void ListMovies(IEnumerable<Movie> movies)
+        {
+            Console.WriteLine("List of Movies:");
+
+            foreach (Movie movie in movies)
+            {
+                Console.WriteLine($"Movie Title: {movie.Title},  Genre: {movie.Genre?.GenreDescr ?? "N/A"},  Release Year: {movie.ReleaseYear}");
+            }
+        }
+
+
+        public static void WriteMoviesToCsv(IEnumerable<Movie> movies)
+        {
+            using (var streamWriter = new StreamWriter("..\\Movies.csv"))
+            {
+                using (var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
+                {
+
+                    csvWriter.WriteRecords(movies);
+                }
+            }
+        }
+
+
+        public static void ListObjects<TOutputObj>(IEnumerable<TOutputObj> objectsToShow)
+            where TOutputObj : class
+        {
+            PropertyInfo[] props = typeof(TOutputObj).GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+            StringBuilder objectStringBuilder = new StringBuilder();
+
+            foreach (TOutputObj obj in objectsToShow)
+            {
+                foreach (PropertyInfo property in props)
+                {
+                    objectStringBuilder.Append($"{property.Name}: {property.GetValue(obj)}, ");
+                }
+
+                Console.WriteLine(objectStringBuilder.ToString().Trim(',', ' '));
+            }
+        }
+    }
+}
---
+++ BlockBusterConsole/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using BlockBuster.Models;
+
+namespace BlockBuster.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> movies = new List<Movie>();
+
+
+            Console.WriteLine("How would you like the output? (CSV or Console)");
+            string outputType = Console.ReadLine()?.ToUpper();
+
+            if (outputType != "CSV" && outputType != "CONSOLE")
+            {
+                Console.WriteLine("Invalid choice. Please enter CSV or Console.");
+                return;
+            }
+
+            Console.WriteLine("Which function do you want to run? (GetAllMovies, GetMoviesByGenre, GetMoviesByDirectorLastName)");
+            string function = Console.ReadLine();
+
+            string parameter = "";
+            if (function == "GetMoviesByGenre" || function == "GetMoviesByDirectorLastName")
+            {
+                Console.WriteLine("Enter the parameter (e.g., Genre name or Director's Last Name):");
+                parameter = Console.ReadLine();
+            }
+
+
+            switch (function)
+            {
+                case "GetAllMovies":
+                    movies = BasicFunctions.GetAllMovies();
+                    break;
+                case "GetMoviesByGenre":
+                    movies = BasicFunctions.GetMoviesByGenre(parameter);
+                    break;
+                case "GetMoviesByDirectorLastName":
+                    movies = BasicFunctions.GetMoviesByDirectorLastName(parameter);
+                    break;
+                default:
+                    Console.WriteLine("Invalid function name.");
+                    return;
+            }
+
+            if (movies.Count == 0)
+            {
+                Console.WriteLine("No movies found.");
+                return;
+            }
+
+            if (outputType == "CSV")
+                ConsoleUtils.WriteMoviesToCsv(movies);
+            else
+                ConsoleUtils.ListMovies(movies);
+        }
+    }
+}
